e04ba02fa3a4514c86aa3ab2a84f3d9eddb32215
smanihwr <smanihwr@gmail.com>
2018-12-20 12:04:53 -0600

    first commit

---
+++ server/config/testing.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // disable logging for testing
+  logging: false
+};
---
+++ server/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var api = require('./api/api');
+
+// setup the app middlware
+require('./middleware/appMiddlware')(app);
+
+// setup the api
+app.use('/api', api);
+
+// set up global error handling
+app.use((err, req, res, next) => {
+    if(err) {
+        console.log(err.message);
+        res.status(500).send(err);
+    }
+});
+
+// export the app for testing
+module.exports = app;
