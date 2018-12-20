804bebb81cf0eda553975b21d497abe5c3ca6e04
smanihwr <smanihwr@gmail.com>
2018-12-20 14:28:59 -0600

    end of exercise 8

---
+++ server/api/post/postModel.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    // array of categories
+    categories: [{
+        type: Schema.Types.ObjectId,
+        ref: 'category'
+    }]
+});
+
+module.exports = mongoose.model('post', PostSchema);
