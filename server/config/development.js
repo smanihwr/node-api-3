3b73e987d307249ca31e33d6637329f7453041a8
smanihwr <smanihwr@gmail.com>
2018-12-21 08:45:17 -0600

    end of exercise 11

--- server/config/development.js
+++ server/config/development.js
@@ -1,7 +1,8 @@
 module.exports = {
-    // enabled logging for development
-    logging: true,
-    db: {
-        url: 'mongodb+srv://user:password@cluster0-gofqg.mongodb.net/nodeblog?retryWrites=true'
-    }
+  // enabled logging for development
+  logging: true,
+  seed: true,
+  db: {
+      url: 'mongodb+srv://user:pwd@cluster0-gofqg.mongodb.net/nodeblog?retryWrites=true'
+  }
 };
